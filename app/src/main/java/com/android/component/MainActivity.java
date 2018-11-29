b5e291d0b5c21e3e54857834ec56f0f4a842cccf
kylingo <studiotang.cn@gmail.com>
2018-11-29 12:26:10 +0800

    Modify test aar module

--- app/src/main/java/com/android/component/MainActivity.java
+++ app/src/main/java/com/android/component/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.component;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.android.component.library.util.ToastUtils;
 import com.android.component.sdk.SdkFragment;
 import com.me.ui.library.sample.SampleActivity;
 
@@ -21,5 +22,6 @@ public class MainActivity extends SampleActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ToastUtils.show(this, getString(R.string.merge_sdk));
     }
 }
